b42f0dbd1fc78160c4b35845cac003e78329516b
Redeemer Dugbazah <dugbazahredeemer@gmail.com>
2024-02-27 19:16:13 +0000

    Fix: Responsive to phone.

--- src/component/ui/Card.jsx
+++ src/component/ui/Card.jsx
@@ -16,32 +16,42 @@ const PhishedCard = ({
   showNextArrow,
 }) => {
   return (
-    <div className="bg-[rgb(255,255,255)] w-full h-[100vh] flex flex-col justify-center items-center">
+    <div className="w-full h-[100vh] flex flex-col justify-center items-center">
       <div className="flex gap-[42px] items-center">
-        <Link to={prevuesPage}>
+        <Link
+          to={prevuesPage}
+          className=" max-sm:absolute max-sm:bottom-9 max-sm:left-[40%] -translate-x-[40%]"
+        >
           <IoIosArrowDropleftCircle
-            className={`hover:opacity-[0.9] ` + showPrevuesArrow}
+            className={`hover:text-[#212d43]` + showPrevuesArrow}
             size={37}
             color="#243654"
           />
         </Link>
-        <div className="h-[700px] bg-white shadow-[0px_5px_15px_rgba(0,0,0,0.35)] rounded-lg w-[440px]">
-          <div className="h-[285px] border-b rounded-md flex justify-center items-center">
-            <img className="object-cover" src={image} alt="" />
+        <div className="sm:h-[700px] max-sm:max-h-[700px] bg-white shadow-[0px_5px_15px_rgba(0,0,0,0.35)] rounded-lg sm:w-[440px] max-sm:max-w-[440px] max-sm:mx-6">
+          <div className="sm:h-[285px] max-sm:h-[285px] border-b rounded-md flex justify-center items-center">
+            <img
+              className="max-sm:h-full max-sm:w-full max-sm:object-cover"
+              src={image}
+              alt=""
+            />
           </div>
-          <div className="text-center py-[55px] px-[20px]">
-            <h4 className="text-[#969191] text-sm pb-[21px] font-[500]">
+          <div className="text-center sm:py-[55px] sm:px-[20px] max-sm:px-7 max-sm:py-10 max-sm:flex max-sm:flex-col gap-10">
+            <h4 className="text-[#969191] text-sm sm:pb-[21px] font-[500]">
               {title}
             </h4>
-            <h3 className="text-[#243654] font-[500] text-[1.8rem] leading-9 pb-[50px]">
+            <h3 className="text-[#243654] font-[500] text-[1.8rem] leading-9 sm:pb-[50px]">
               {boldContent}
             </h3>
             <div className="text-[#807D7D] text-xs">{simeContent}</div>
           </div>
         </div>
-        <Link to={nextPage}>
+        <Link
+          to={nextPage}
+          className=" max-sm:absolute max-sm:bottom-9 max-sm:right-[40%] translate-x-[40%]"
+        >
           <IoIosArrowDroprightCircle
-            className={`hover:opacity-[0.9] ` + showNextArrow}
+            className={`hover:text-[#212d43] ` + showNextArrow}
             size={37}
             color="#243654"
           />
